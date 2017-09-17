67514e625c323f856089a2263dd6cf65d262af2a
Jerry Wiltse <jerrywiltse@gmail.com>
2017-09-17 11:29:55 -0400

    Updated version to 1.65.1

--- conanfile.py
+++ conanfile.py
@@ -2,37 +2,37 @@ from conans import ConanFile, tools, os
 
 class BoostTestConan(ConanFile):
     name = "Boost.Test"
-    version = "1.64.0"
+    version = "1.65.1"
     generators = "boost" 
     settings = "os", "arch", "compiler", "build_type"
     short_paths = True
     url = "https://github.com/bincrafters/conan-boost-test"
-    description = "Please visit http://www.boost.org/doc/libs/1_64_0/libs/libraries.htm"
+    description = "Please visit http://www.boost.org/doc/libs/1_65_1/libs/libraries.htm"
     license = "www.boost.org/users/license.html"
     lib_short_names = ["test"]
     options = {"shared": [True, False]}
     default_options = "shared=False"
     build_requires = "Boost.Generator/0.0.1@bincrafters/testing" 
-    requires =  "Boost.Algorithm/1.64.0@bincrafters/testing", \
-                      "Boost.Assert/1.64.0@bincrafters/testing", \
-                      "Boost.Bind/1.64.0@bincrafters/testing", \
-                      "Boost.Config/1.64.0@bincrafters/testing", \
-                      "Boost.Core/1.64.0@bincrafters/testing", \
-                      "Boost.Detail/1.64.0@bincrafters/testing", \
-                      "Boost.Exception/1.64.0@bincrafters/testing", \
-                      "Boost.Function/1.64.0@bincrafters/testing", \
-                      "Boost.Io/1.64.0@bincrafters/testing", \
-                      "Boost.Iterator/1.64.0@bincrafters/testing", \
-                      "Boost.Mpl/1.64.0@bincrafters/testing", \
-                      "Boost.Numeric_Conversion/1.64.0@bincrafters/testing", \
-                      "Boost.Optional/1.64.0@bincrafters/testing", \
-                      "Boost.Preprocessor/1.64.0@bincrafters/testing", \
-                      "Boost.Range/1.64.0@bincrafters/testing", \
-                      "Boost.Smart_Ptr/1.64.0@bincrafters/testing", \
-                      "Boost.Static_Assert/1.64.0@bincrafters/testing", \
-                      "Boost.Timer/1.64.0@bincrafters/testing", \
-                      "Boost.Type_Traits/1.64.0@bincrafters/testing", \
-                      "Boost.Utility/1.64.0@bincrafters/testing"
+    requires =  "Boost.Algorithm/1.65.1@bincrafters/testing", \
+                      "Boost.Assert/1.65.1@bincrafters/testing", \
+                      "Boost.Bind/1.65.1@bincrafters/testing", \
+                      "Boost.Config/1.65.1@bincrafters/testing", \
+                      "Boost.Core/1.65.1@bincrafters/testing", \
+                      "Boost.Detail/1.65.1@bincrafters/testing", \
+                      "Boost.Exception/1.65.1@bincrafters/testing", \
+                      "Boost.Function/1.65.1@bincrafters/testing", \
+                      "Boost.Io/1.65.1@bincrafters/testing", \
+                      "Boost.Iterator/1.65.1@bincrafters/testing", \
+                      "Boost.Mpl/1.65.1@bincrafters/testing", \
+                      "Boost.Numeric_Conversion/1.65.1@bincrafters/testing", \
+                      "Boost.Optional/1.65.1@bincrafters/testing", \
+                      "Boost.Preprocessor/1.65.1@bincrafters/testing", \
+                      "Boost.Range/1.65.1@bincrafters/testing", \
+                      "Boost.Smart_Ptr/1.65.1@bincrafters/testing", \
+                      "Boost.Static_Assert/1.65.1@bincrafters/testing", \
+                      "Boost.Timer/1.65.1@bincrafters/testing", \
+                      "Boost.Type_Traits/1.65.1@bincrafters/testing", \
+                      "Boost.Utility/1.65.1@bincrafters/testing"
 
                       #algorithm9 assert1 bind3 config0 core2 detail5 exception5 function5 io1 iterator5 mpl5 numeric~conversion6 optional5 preprocessor0 range7 smart_ptr4 static_assert1 timer4 type_traits3 utility5
 
