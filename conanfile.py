4732940599fc59016e4b89e672e41a57814da5d2
Rene Rivera <grafikrobot@gmail.com>
2017-12-06 08:13:49 -0600

    Require generator.

--- conanfile.py
+++ conanfile.py
@@ -4,15 +4,12 @@ from conans import ConanFile, tools
 class BoostTestConan(ConanFile):
     name = "Boost.Test"
     version = "1.65.1"
-    url = "https://github.com/bincrafters/conan-boost-test"
-    description = "Please visit http://www.boost.org/doc/libs/1_65_1/libs/libraries.htm"
-    license = "www.boost.org/users/license.html"
 
-    settings = "os", "arch", "compiler", "build_type"
     options = {"shared": [True, False]}
     default_options = "shared=False"
 
     requires = \
+        "Boost.Generator/1.65.1@bincrafters/testing", \
         "Boost.Algorithm/1.65.1@bincrafters/testing", \
         "Boost.Assert/1.65.1@bincrafters/testing", \
         "Boost.Bind/1.65.1@bincrafters/testing", \
@@ -39,20 +36,19 @@ class BoostTestConan(ConanFile):
 
     # BEGIN
 
+    url = "https://github.com/bincrafters/conan-boost-test"
+    description = "Please visit http://www.boost.org/doc/libs/1_65_1"
+    license = "www.boost.org/users/license.html"
     short_paths = True
     build_requires = "Boost.Generator/1.65.1@bincrafters/testing"
     generators = "boost"
+    settings = "os", "arch", "compiler", "build_type"
 
-    def package_id(self):
-        if self.is_header_only:
-            self.info.header_only()
-
-    # pylint: disable=unused-import
     @property
     def env(self):
         try:
             with tools.pythonpath(super(self.__class__, self)):
-                import boostgenerator # pylint: disable=F0401
+                import boostgenerator  # pylint: disable=F0401
                 boostgenerator.BoostConanFile(self)
         except:
             pass
